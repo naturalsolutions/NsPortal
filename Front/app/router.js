0d69f2b390fe34f2e8bda74700f2d5004623804b
JWNS <david_lassagne@natural-solutions.eu>
2016-09-19 17:07:44 +0200

    fixed issue on hompage portal

--- Front/app/router.js
+++ Front/app/router.js
@@ -35,6 +35,7 @@ return Marionette.AppRouter.extend({
       window.app.rootView.rgMain.show(new LytLogin());
       Backbone.history.navigate('login', {trigger: true});
     }).always(function(){
+      $("body").css('background-image', 'none');
     });
   },
 
