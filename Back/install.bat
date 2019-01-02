855ee629edfb6ec7769856a7a1af5e0b25537eb2
MrJeje_ <jeremy.leriche22@gmail.com>
2019-01-02 13:56:58 +0100

    update on install procedure

--- Back/install.bat
+++ Back/install.bat
@@ -2,10 +2,8 @@
 :: Requires Python 3.4.1, conda and conda-build
 :: Minimum version of Pandas should be 0.15.0
 
-conda install pyodbc
-conda install sqlalchemy
-conda install zope.interface
-pip install pyramid_jwtauth
-
-
-python setup.py install
+call python -m pip install --user virtualenv
+call python -m venv .\portalvenv
+call portalvenv\Scripts\activate
+call python -m pip install --upgrade pip setuptools
+call python setup.py install
