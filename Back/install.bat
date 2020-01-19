74631c49085e906f20d99c28e3bd8c8d0762edca
jvitus <jeanvitus.albertini@gmail.com>
2020-01-19 02:27:48 +0100

    new install script

--- Back/install.bat
+++ Back/install.bat
@@ -1,9 +1,4 @@
-:: ecoReleve-Sensor installation script
-:: Requires Python 3.4.1, conda and conda-build
-:: Minimum version of Pandas should be 0.15.0
+:: Requires Python 3.7 tested on python 3.8.1
 
-call python -m pip install --user virtualenv
-call python -m venv .\portalvenv
-call portalvenv\Scripts\activate
-call python -m pip install --upgrade pip setuptools
-call python setup.py install
+call python -m venv env
+call .\upgradeinstall.bat
