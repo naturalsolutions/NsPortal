59e9f97cf547136974ba6c3687614776d700d9de
jvitus <jeanvitus.albertini@gmail.com>
2020-01-20 04:29:27 +0100

    new app starting

--- Back/ns_portal/__init__.py
+++ Back/ns_portal/__init__.py
@@ -5,5 +5,8 @@ from pyramid.renderers import JSON
 def main(global_config, **settings):
     with Configurator(settings=settings) as config:
         config.add_renderer('json', JSON(indent=4))
+        config.include('ns_portal.routes')
+        config.include('ns_portal.core.resources')
+        config.include('ns_portal.resources')
 
     return config.make_wsgi_app()
