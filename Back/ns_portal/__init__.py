1fb95ad1ab2c85626116a12f93fe12b6b7eb8e8f
jvitus <jeanvitus.albertini@gmail.com>
2020-01-23 08:53:40 +0100

    include database and utils in app

--- Back/ns_portal/__init__.py
+++ Back/ns_portal/__init__.py
@@ -6,7 +6,9 @@ def main(global_config, **settings):
     with Configurator(settings=settings) as config:
         config.add_renderer('json', JSON(indent=4))
         config.include('ns_portal.routes')
+        config.include('ns_portal.database')
         config.include('ns_portal.core.resources')
         config.include('ns_portal.resources')
+        config.include('ns_portal.utils')
 
     return config.make_wsgi_app()
