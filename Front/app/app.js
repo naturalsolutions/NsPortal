8ca04d46c3c6eee6e9be07a4942794c0a773ff4a
jvitus <jeanvitus.albertini@gmail.com>
2020-01-31 08:45:56 +0100

    remove search and replace in query string

--- Front/app/app.js
+++ Front/app/app.js
@@ -24,23 +24,23 @@ Controller, config) {
   app.on('start', function() {
     var _this = this;
     var params={};
-    window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(str,key,value) {
-        params[key] = value;
-      }
-    );
-    var val;
-    if(params.img){
-      val = params.img;
-    }
-    if (params.image){
-      val = params.image;
-    }
-    var url = config.coreUrl + 'site';
-    if(val=="0" || val=="false" || val == false){
-      url = url + '?noimage=true';
-    }
+    // window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(str,key,value) {
+    //     params[key] = value;
+    //   }
+    // );
+    // var val;
+    // if(params.img){
+    //   val = params.img;
+    // }
+    // if (params.image){
+    //   val = params.image;
+    // }
+    // var url = config.coreUrl + 'site';
+    // if(val=="0" || val=="false" || val == false){
+    //   url = url + '?noimage=true';
+    // }
     var Patern = Backbone.Model.extend({
-      urlRoot: url
+      urlRoot: config.coreUrl + 'site'
     });
     var model = new Patern();
     model.fetch({
