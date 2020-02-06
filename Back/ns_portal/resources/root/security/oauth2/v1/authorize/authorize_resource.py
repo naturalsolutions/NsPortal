d0bceec6b8901d765de05f83ac4899722c257985
jvitus <jeanvitus.albertini@gmail.com>
2020-02-06 08:42:59 +0100

    change query for validateschema

--- Back/ns_portal/resources/root/security/oauth2/v1/authorize/authorize_resource.py
+++ Back/ns_portal/resources/root/security/oauth2/v1/authorize/authorize_resource.py
@@ -4,26 +4,39 @@ from ns_portal.core.resources import (
 from marshmallow import (
     Schema,
     fields,
+    EXCLUDE,
     ValidationError
 )
 from pyramid.security import (
     Allow,
-    Authenticated
+    Authenticated,
+    _get_authentication_policy
 )
 from ns_portal.database.main_db import (
-    TApplications
+    TInstance,
+    TApplications,
+    TAutorisations,
+    TUsers,
+    TRoles,
+    TSite
 )
 from sqlalchemy.orm.exc import (
     MultipleResultsFound
 )
 from ns_portal.utils import (
-    getOauth2CodeWithSecret
+    getCodeToken
+)
+from pyramid.httpexceptions import (
+    HTTPFound
 )
 
 
 class authorizeSchema(Schema):
     client_id = fields.String(required=True)
     redirect_uri = fields.String(required=True)
+
+    class Meta:
+        unknown = EXCLUDE
 
 
 class AuthorizeResource(MetaEndPointResource):
@@ -36,13 +49,55 @@ class AuthorizeResource(MetaEndPointResource):
         client_id = data.get('client_id')
         redirect_uri = data.get('redirect_uri')
 
-        query = self.request.dbsession.query(TApplications)
-        query = query.filter(
-            TApplications.TApp_Name == client_id,
-            TApplications.TApp_Name.like(f'%{redirect_uri}%')
+        policy = _get_authentication_policy(self.request)
+        tsiteName = getattr(policy, 'TSit_Name')
+        userId = self.request.authenticated_userid.get('TUse_PK_ID')
+
+        colsToRet = [
+            TInstance.TIns_PK_ID,
+            TInstance.TIns_Label,
+            TInstance.TIns_ApplicationPath,
+            TInstance.TIns_Theme,
+            TInstance.TIns_Database,
+            TInstance.TIns_Order,
+            TInstance.TIns_ReadOnly,
+            TApplications.TApp_Description,
+            TRoles.TRol_Label,
+            TUsers.TUse_PK_ID,
+            TSite.TSit_Name,
+            TSite.TSit_Project,
+            TSite.TSit_ImagePathMainLogo,
+            TSite.TSit_ImagePathMainMenu,
+            TAutorisations.TUse_Observer
+        ]
+
+        VAllUsersApplications = self.request.dbsession.query(TInstance)
+        VAllUsersApplications = VAllUsersApplications.join(TApplications)
+        VAllUsersApplications = VAllUsersApplications.join(
+            TAutorisations,
+            TInstance.TIns_PK_ID == TAutorisations.TAut_FK_TInsID
+            )
+        VAllUsersApplications = VAllUsersApplications.join(TRoles)
+        VAllUsersApplications = VAllUsersApplications.join(TUsers)
+        VAllUsersApplications = VAllUsersApplications.join(
+            TSite,
+            TInstance.TIns_FK_TSitID == TSite.TSit_PK_ID
+            )
+
+        VAllUsersApplications = VAllUsersApplications.with_entities(*colsToRet)
+
+        VAllUsersApplications = VAllUsersApplications.filter(
+                (TSite.TSit_Name == tsiteName),
+                (TUsers.TUse_PK_ID == userId),
+                (TRoles.TRol_Label != 'Interdit'),
+                (TInstance.TIns_Label == client_id),
+                (TInstance.TIns_ApplicationPath == redirect_uri)
+            )
+        VAllUsersApplications = VAllUsersApplications.order_by(
+            TInstance.TIns_Order
             )
         try:
-            res = query.one_or_none()
+            res = VAllUsersApplications.one_or_none()
         except MultipleResultsFound:
             raise MultipleResultsFound()
         if res:
@@ -63,9 +118,8 @@ class AuthorizeResource(MetaEndPointResource):
 
         self.validateSchema(data=reqParams)
 
-        code = getOauth2CodeWithSecret(
+        code = getCodeToken(
             idUser=self.request.authenticated_userid.get('TUse_PK_ID'),
-            client_id=reqParams.get('client_id'),
             request=self.request
         )
 
